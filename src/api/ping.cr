a6b4b4861a6c375486b22fb8d8309f71698f8ece
Fernando Briano <fernando@picandocodigo.net>
2024-01-30 12:37:05 +0000

    [API] Adds ping endpoint

---
+++ src/api/ping.cr
@@ -0,0 +1,50 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Auto generated from build hash f284cc16f4d4b4289bc679aa1529bb504190fe80
+# @see https://github.com/elastic/elasticsearch/tree/main/rest-api-spec
+#
+module Elasticsearch
+  module API
+    module Actions
+      # Returns whether the cluster is running.
+      #
+      # See [https://www.elastic.co/guide/en/elasticsearch/reference/current/index.html](https://www.elastic.co/guide/en/elasticsearch/reference/current/index.html)
+      #
+      def ping(arguments = Hash(Symbol, String).new)
+        arguments = arguments.clone
+        headers = arguments.delete(:headers) || Hash(Symbol, String).new
+
+        body = nil
+
+        method = Elasticsearch::API::HTTP_HEAD
+        path = ""
+        params = Hash(String, String).new
+
+        begin
+          perform_request(method, path, params, body, headers).status == 200
+        rescue e
+          if e.class.to_s =~ /NotFound|ConnectionFailed/ || e.message =~ /Not *Found|404|ConnectionFailed/i
+            false
+          else
+            raise e
+          end
+        end
+      end
+    end
+  end
+end
